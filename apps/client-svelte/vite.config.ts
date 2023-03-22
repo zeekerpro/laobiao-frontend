0095ff1a736430b116766a04633b46df734ec08d
zeeker <zeekerpro@gmail.com>
2023-03-22 18:15:17 +0800

    feat: test deploy on vercel

--- apps/client-svelte/vite.config.ts
+++ apps/client-svelte/vite.config.ts
@@ -1,18 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { VitePWA } from "vite-plugin-pwa";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
 	plugins: [
     sveltekit(),
     mkcert(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
   ],
   server: {
     https: true
