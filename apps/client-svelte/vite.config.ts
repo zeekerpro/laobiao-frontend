dd680c97dd3ca001652fe6ec9bae7cc42f588881
zeeker <zeekerpro@gmail.com>
2023-03-22 16:37:48 +0800

    feat: enable https service in the dev environment of client-svelte

--- apps/client-svelte/vite.config.ts
+++ apps/client-svelte/vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { VitePWA } from "vite-plugin-pwa";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
 	plugins: [
     sveltekit(),
+    mkcert(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
@@ -12,6 +14,9 @@ export default defineConfig({
       }
     })
   ],
+  server: {
+    https: true
+  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
