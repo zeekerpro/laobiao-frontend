7db17861ab4442b68f30b9dcdee950800ecb085f
zeeker <zeekerpro@gmail.com>
2023-03-28 21:50:48 +0800

    feat: add no-op.js for service worker to recover when fireback

---
+++ apps/client-svelte/src/service-worker/onop.ts
@@ -0,0 +1,26 @@
+// deploy a no-op service worker, and then immediately replace it with the real one
+// just replace index.ts with onop.ts and then deploy
+
+// https://developer.chrome.com/docs/workbox/remove-buggy-service-workers/
+
+// sw.js
+self.addEventListener('install', () => {
+  // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  // Optional: Get a list of all the current open windows/tabs under
+  // our service worker's control, and force them to reload.
+  // This can "unbreak" any open windows/tabs as soon as the new
+  // service worker activates, rather than users having to manually reload.
+  self.clients.matchAll({
+    type: 'window'
+  }).then(windowClients => {
+    windowClients.forEach((windowClient) => {
+      windowClient.navigate(windowClient.url);
+    });
+  });
+});
