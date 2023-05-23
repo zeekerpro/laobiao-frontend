6a3ec5caa1e5c565491125e462330661593cf17c
zeeker <zeekerpro@gmail.com>
2023-05-23 12:08:03 +0800

    style: custom page out transition

--- apps/client-svelte/src/routes/+layout.svelte
+++ apps/client-svelte/src/routes/+layout.svelte
@@ -2,8 +2,26 @@
   import "@styles/app.css";
   import ThemeSwitcher from "@components/ThemeSwitcher.svelte";
   import { fly } from "svelte/transition";
+  import { cubicOut } from "svelte/easing";
 
   export let data
+
+
+  function pageOut(
+    node: Element,
+    { delay = 0, duration = 300, easing = cubicOut, direction = "center center" } = {}
+  ) {
+    return {
+      delay,
+      duration,
+      easing,
+      css: (t :number) => `
+        scale: ${t};
+        transform-origin: ${direction};
+      `
+    }
+  }
+
 </script>
 
 <main class="flow-root">
@@ -14,8 +32,8 @@
 
   {#key data.url}
   <div
-    in:fly={{ x: -200, duration: 500, delay: 800 }}
-    out:fly={{ duration: 100 }}
+    in:fly={{ x: -200, duration: 500, delay: 300 }}
+    out:pageOut={{ duration: 300, direction: "bottom right" }}
     >
     <slot></slot>
   </div>
