ebd46ac9c334c9474f10260e01c9d6422b5faab7
zeeker <zeekerpro@gmail.com>
2023-06-06 18:21:23 +0800

    Title: Fix signed in status check on client layout
    Description: Previously, the client layout did not correctly check for signed
     in status, causing issues with displaying user-specific content. This commit
     fixes the check by subscribing to the isLoggedIn observable and updating the
     logged status accordingly. Additionally, it updates the session with the user
     data if successfully signed in, and redirects the user to the sign in page
     if not.

--- apps/client-svelte/src/routes/+layout.ts
+++ apps/client-svelte/src/routes/+layout.ts
@@ -10,13 +10,17 @@ export const load = ( async ({route, url}) => {
 
   // check signed status at first open page
   if(config.authWhiteList.includes(route.id || "")) { return }
-  if(browser && !isLoggedIn) {
+  let logged = true;
+  const unsubscribe = isLoggedIn.subscribe(value => logged = !!value)
+  if(browser && !logged) {
     const ret = await userService.me()
     if(ret.isSuccess) {
       log.layout("signed success by token")
       session.update(n => ({...n, user: ret.data}))
+      unsubscribe()
     }else{
       log.layout("unsigned")
+      unsubscribe()
       throw redirect(307, "/signin")
     }
   }
