7fc95b41ae651589154a2a6735dd39930e2afa42
zeeker <zeekerpro@gmail.com>
2023-06-07 10:25:04 +0800

    Git commit message:
    refactor(layout): add loading state and async function for checking sign-in
     status
    
    Summary: The commit refactors the layout component by adding a loading state
     and an async function to check the sign-in status. This improves the user experience
     by showing a loading spinner while the sign-in request is being made. The function
     also checks the sign-in status when the page is first opened.

--- apps/client-svelte/src/routes/+layout.svelte
+++ apps/client-svelte/src/routes/+layout.svelte
@@ -5,12 +5,42 @@
   import { navigating } from "$app/stores";
   import config  from "./config.json";
   import { guard } from "@utils/client/routeMonitor";
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+  import { session, isLoggedIn } from "@stores/session";
+  import { userService } from "@apis";
+  import { log } from "@utils/log";
+  import { goto } from "$app/navigation";
+
+  let isLoading = true
 
   // route guard
   $: if($navigating) {
     // @ts-ignore
     guard($navigating, config.authWhiteList)
   }
+
+  async function checkIsSngined() {
+    isLoading = true
+    // check signed status at first open page
+    if(config.authWhiteList.includes($page.route.id || "")) { isLoading = false; return }
+    if(!$isLoggedIn) {
+      const ret = await userService.me()
+      if(ret.isSuccess) {
+        log.layout("signed success by token")
+        $session.user = ret.data
+        isLoading = false
+      }else{
+        log.layout("unsigned, goto signin page")
+        isLoading = false
+        goto("/signin")
+      }
+    }
+  }
+
+  onMount(() => {
+    checkIsSngined()
+  })
 
 </script>
 
@@ -20,8 +50,10 @@
     <ThemeSwitcher />
   </div>
 
-  <PageTransition>
-    <slot></slot>
-  </PageTransition>
+  {#if !isLoading }
+    <PageTransition>
+      <slot></slot>
+    </PageTransition>
+  {/if }
 
 </main>
--- apps/client-svelte/src/routes/+layout.ts
+++ apps/client-svelte/src/routes/+layout.ts
@@ -1,29 +1,6 @@
-import { userService } from "@apis";
-import { redirect } from "@sveltejs/kit";
-import { log } from "@utils/log";
 import type { LayoutLoad } from "./$types";
-import { browser } from "$app/environment";
-import config from "./config.json";
-import { session, isLoggedIn } from "@stores/session";
 
 export const load = ( async ({route, url}) => {
-
-  // check signed status at first open page
-  if(config.authWhiteList.includes(route.id || "")) { return }
-  let logged = false;
-  const unsubscribe = isLoggedIn.subscribe(value => logged = !!value)
-  if(browser && !logged) {
-    const ret = await userService.me()
-    if(ret.isSuccess) {
-      log.layout("signed success by token")
-      session.update(n => ({...n, user: ret.data}))
-      unsubscribe()
-    }else{
-      log.layout("unsigned")
-      unsubscribe()
-      throw redirect(307, "/signin")
-    }
-  }
 
 }) satisfies LayoutLoad;
 
