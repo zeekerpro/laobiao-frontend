ed5ec39c2a709e8e335338c5dd6f080c97d27243
zeeker <zeekerpro@gmail.com>
2023-06-08 14:28:06 +0800

    Add @iconify/svelte to devDependencies for icon support in TabBar component .

--- apps/client-svelte/src/lib/components/TabBar.svelte
+++ apps/client-svelte/src/lib/components/TabBar.svelte
@@ -1,21 +1,34 @@
 <script lang="ts">
 import {page} from "$app/stores"
+import Icon from "@iconify/svelte"
 
 let tabs = [
   {
     id: "home",
     label: "home",
     path: "/",
+    icon: {
+      active: "mdi:home",
+      inactive: "mdi:home-outline"
+    }
   },
   {
     id: "chat",
     label: "chat",
     path: "/chat",
+    icon: {
+      active: "mdi:chat",
+      inactive: "mdi:chat-outline"
+    }
   },
   {
     id: "profile",
     label: "profile",
     path: "/profile",
+    icon: {
+      active: "mdi:account",
+      inactive: "mdi:account-outline"
+    }
   }
 ]
 
@@ -23,12 +36,18 @@ $: currentPath = $page.url.pathname
 
 </script>
 
-<footer class=" fixed bottom-0 w-full ">
-  <div class="tabs flex justify-between bg-base-100">
+<footer class=" fixed bottom-0 w-full">
+  <div class="tabs flex justify-between border border-base-300">
     {#each tabs as tab}
-      <a class="tab tab-lifted grow {currentPath == tab.path ? 'tab-active' : ''}" href={tab.path} >
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-        <span>{tab.label}</span>
+      <a class="tab
+        {currentPath == tab.path ? 'tab-active' : ''}
+        {currentPath == tab.path ? 'text-primary-focus' : ''}
+        grow h-auto
+        flex flex-col items-center justify-center flex-nowrap
+        "
+        href={tab.path}
+        >
+        <Icon icon="{currentPath == tab.path ? tab.icon.active: tab.icon.inactive}" class="text-3xl my-1" />
       </a>
     {/each}
   </div>
