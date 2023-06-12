c730d1252ca4d60f481a8e77d2b2eda6b1edb3be
zeeker <zeekerpro@gmail.com>
2023-06-12 13:03:26 +0800

    Title: Add preloading spinner in layout component
    Description: This commit adds a preloading spinner in the layout component for
     better user experience. It checks whether the user is logged in or not and
     handles navigation accordingly. The implementation uses the `isLoading` flag
     to conditionally render the spinner and the `onMount` lifecycle hook to check
     the user's authentication status. The code is added in `Preloading.svelte`
     and `+layout.svelte`.

--- apps/client-svelte/src/lib/components/layout/Preloading.svelte
+++ apps/client-svelte/src/lib/components/layout/Preloading.svelte
@@ -1,7 +1,43 @@
 <script>
   import LogoAnimation from "$components/layout/LogoAnimation.svelte";
   import ThemeSwitcher from "$components/ThemeSwitcher.svelte";
+  import { onMount } from "svelte";
+  import { session, isLoggedIn } from "$stores/session";
+  import { log } from "$utils/log";
+  import { goto } from "$app/navigation";
+  import User from "$models/User";
+  import { authWhiteList } from "$configs";
+  import { page } from "$app/stores";
+
+  export let isLoading = true;
+
+  async function checkIsSigned() {
+    isLoading = true
+    // check signed status at first open page
+    if(authWhiteList.includes($page.route.id || "")) { isLoading = false; return }
+    if(!$isLoggedIn) {
+      const ret = await User.me()
+      if(ret.isSuccess) {
+        log.layout("signed success by token")
+        $session.user = ret.data
+      }else{
+        log.layout("unsigned, goto signin page")
+        isLoading = false
+        goto("/signin")
+      }
+    }
+    isLoading = false
+  }
+
+  onMount(() => {
+    checkIsSigned()
+  })
+
 </script>
 
-<LogoAnimation class="h-screen flex justify-center" dancing={true} />
-<ThemeSwitcher class="fixed top-3 right-3" />
+{#if isLoading}
+  <div class="h-screen w-screen flex justify-center items-center absolute top-0 left-0 bg-base-100">
+    <LogoAnimation class="h-screen flex justify-center" dancing={true} />
+    <ThemeSwitcher class="fixed top-3 right-3" />
+  </div>
+{/if}
--- apps/client-svelte/src/routes/+layout.svelte
+++ apps/client-svelte/src/routes/+layout.svelte
@@ -5,48 +5,17 @@
   import MainContent from "$components/layout/MainContent.svelte";
   import { authWhiteList } from "$configs";
   import { guard } from "$utils/client/routeMonitor";
-  import { onMount } from "svelte";
   import { page } from "$app/stores";
-  import { session, isLoggedIn } from "$stores/session";
-  import { log } from "$utils/log";
-  import { beforeNavigate, goto } from "$app/navigation";
-  import User from "$models/User";
-
-  let isLoading = true
+  import { beforeNavigate } from "$app/navigation";
 
   $: isAuthPage = $page.route.id && authWhiteList.includes($page.route.id)
 
   beforeNavigate(guard)
 
-  async function checkIsSigned() {
-    isLoading = true
-    // check signed status at first open page
-    if(authWhiteList.includes($page.route.id || "")) { isLoading = false; return }
-    if(!$isLoggedIn) {
-      const ret = await User.me()
-      if(ret.isSuccess) {
-        log.layout("signed success by token")
-        $session.user = ret.data
-      }else{
-        log.layout("unsigned, goto signin page")
-        isLoading = false
-        goto("/signin")
-      }
-    }
-    isLoading = false
-  }
-
-  onMount(() => {
-    checkIsSigned()
-  })
-
 </script>
 
 <div class="flow-root">
 
-  {#if isLoading}
-    <Preloading />
-  {/if}
 
   {#if isAuthPage}
     <PageTransition>
@@ -57,5 +26,7 @@
       <slot></slot>
     </MainContent>
   {/if}
+
+  <Preloading />
 
 </div>
