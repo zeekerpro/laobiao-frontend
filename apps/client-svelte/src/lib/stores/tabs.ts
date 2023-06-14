2dd527659b7746bfa30ef13e6b68b82a219f4736
zeeker <zeekerpro@gmail.com>
2023-06-14 19:28:19 +0800

    "Remove unused code from tabs.ts store"

--- apps/client-svelte/src/lib/stores/tabs.ts
+++ apps/client-svelte/src/lib/stores/tabs.ts
@@ -1,9 +1,6 @@
 import { writable, derived } from "svelte/store";
 import { tabs } from "$configs";
 
-// export const activeTabIndex = writable(0);
-// export const previousTabIndex = writable(0);
-
 export const tabsInfo = writable({
   activeTabIndex: 0,
   prevTabIndex: 0,
