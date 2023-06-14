48d06e8f6117b11bd69b1c79311d5395b8823124
zeeker <zeekerpro@gmail.com>
2023-06-15 02:14:21 +0800

    "Refactor Chat.svelte tab button opacity transition"

--- apps/client-svelte/src/lib/components/views/tabs/Chat.svelte
+++ apps/client-svelte/src/lib/components/views/tabs/Chat.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import TabTransition from "$components/layout/tabs/TabTransition.svelte";
   import Icon from "@iconify/svelte"
-  import { activeTab } from "$stores/layouts";
-
-  $: isChatTab = $activeTab?.label === 'chat';
 
 </script>
 
@@ -13,8 +10,7 @@ this is chat page
 
 <button class="btn btn-circle
   text-2xl bg-info
-  transition-opacity duration-500
-  { isChatTab ? 'opacity-100 fixed bottom-32 right-2 ' : 'opacity-0'}
+  fixed bottom-32 right-2
   ">
   <Icon class="" icon="material-symbols:add" />
 </button>
