a5bd953d43db07ef5b0ce432a43e5d52cc69bef4
zeeker <zeekerpro@gmail.com>
2023-06-14 15:35:38 +0800

    Remove beforeNavigate call from Tabs layout

--- apps/client-svelte/src/routes/(tabs)/+layout.svelte
+++ apps/client-svelte/src/routes/(tabs)/+layout.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
   import TabsContainer from "$components/layout/TabsContainer.svelte";
-  import { guard } from "$utils/client/routeMonitor";
-  import { beforeNavigate } from "$app/navigation";
-
-  beforeNavigate(guard)
-
 </script>
 
 <TabsContainer>
