4f3a4be7f56b46fcee5c4d6a6c8f2eed712fc793
zeeker <zeekerpro@gmail.com>
2023-06-15 02:05:24 +0800

    Commit message: "Add delayed display functionality for 'Add Chat' button"

--- apps/client-svelte/src/lib/components/views/tabs/Chat.svelte
+++ apps/client-svelte/src/lib/components/views/tabs/Chat.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
   import TabTransition from "$components/layout/tabs/TabTransition.svelte";
   import Icon from "@iconify/svelte"
+  import { onDestroy, onMount } from "svelte";
+
+  let showAddChat = false;
+
+  let showAddChatTimer :any;
+
+  onMount(() => {
+    showAddChatTimer = setTimeout(() => {
+      showAddChat = true;
+    }, 1000);
+  })
+
+  onDestroy(() => {
+    clearTimeout(showAddChatTimer);
+  })
+
 </script>
 
 <TabTransition>
 
 this is chat page
 
-<button class="btn btn-circle fixed bottom-32 right-2 text-2xl bg-info">
-  <Icon class="" icon="material-symbols:add" />
-</button>
+{#key showAddChat}
+  <button class="btn btn-circle
+    fixed bottom-32 right-2
+    text-2xl bg-info
+    transition-opacity duration-500
+    {showAddChat ? 'opacity-100' : 'opacity-0'}
+    ">
+    <Icon class="" icon="material-symbols:add" />
+  </button>
+{/key}
 
 </TabTransition>
 
